b0f2ac38962e6a8b5b764b5169e6cb9a33a9b387
guptayogansh <yogansh.11@gmail.com>
2019-10-26 21:06:28 +0530

    using external html file

--- Handler.js
+++ Handler.js
@@ -1,20 +1,32 @@
+
 var querystring = require('querystring')
+var fs = require('fs');
+
 function home(response){
 	console.log('Executing home handler');
-	var htmlFile = '<html>'+
-'<head>'+
-	'<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>'+
-'</head>'+
-'<body>'+
-	'<form action="/review" method="post">'+
-		'<textarea name = "text" rows="10" cols="30"></textarea>'+'<br><br>'+
-		'<input type = "submit" value="Submit Text"/>'+	
-	'</form>'+
-'</body>'+
-'</html>';
+// 	var htmlFile = '<html>'+
+// '<head>'+
+// 	'<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>'+
+// '</head>'+
+// '<body>'+
+// 	'<form action="/review" method="post">'+
+// 		'<textarea name = "text" rows="10" cols="30"></textarea>'+'<br><br>'+
+// 		'<input type = "submit" value="Submit Text"/>'+	
+// 	'</form>'+
+// '</body>'+
+// '</html>';
   response.writeHead(200,{'Content-Type':'text/html'});
-  response.write(htmlFile);
-	response.end();
+  // response.write(htmlFile);
+  fs.readFile('./index.html',null,function(error,data){
+  	if(error){
+  		response.writeHead(404);
+  		response.write('File not found');
+  	}else{
+  		response.write(data);
+  	}
+
+  	response.end();
+  });
 
 }   
 
